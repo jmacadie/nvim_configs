ac2535739be5b39f1e8bab740593d6e6251e9594
James MacAdie <james@macadie.co.uk>
2022-11-21 18:01:32 +0000

    Initial Commit

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,31 @@
+-- import nvim-treesitter plugin safely
+local status, treesitter = pcall(require, "nvim-treesitter.configs")
+if not status then
+  return
+end
+
+-- configure treesitter
+treesitter.setup({
+  -- enable syntax highlighting
+  highlight = {
+    enable = true,
+  },
+  -- enable indentation
+  indent = { enable = true },
+  -- enable autotagging (w/ nvim-ts-autotag plugin)
+  autotag = { enable = true },
+  -- ensure these language parsers are installed
+  ensure_installed = {
+    "lua",
+    "vim",
+    "rust",
+    "toml",
+  },
+  -- auto install above language parsers
+  auto_install = true,
+  rainbow = {
+    enable = true,
+    extended_mode = true,
+    max_file_lines = nil,
+  },
+})
